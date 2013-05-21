bd94079682491d0253472ac184993408e2adc282
Paul Springett <paul@springett.me>
2013-05-21 21:53:59 +0100

    Config docs

--- lib/bcx/configuration.rb
+++ lib/bcx/configuration.rb
@@ -1,4 +1,13 @@
 module Bcx
+
+  # Bcx::Configuration
+  # Provides a configuration block for setting up the Bcx client
+  # Example:
+  #
+  #   Bcx.configure do |config|
+  #     config.account = '1234567890'
+  #   end
+  #
   class Configuration
     attr_accessor :account, :api_version
 
