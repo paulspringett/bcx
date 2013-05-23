470c293eb7d90dd02b3abc086d3d5bfced93cbba
Paul Springett <paul@springett.me>
2013-05-23 21:41:46 +0100

    Add docs for projects endpoints

--- lib/bcx/resources/project.rb
+++ lib/bcx/resources/project.rb
@@ -5,14 +5,35 @@ module Bcx
     # Provides access to projects resoource and other nested resources
     # Examples:
     #
-    #   # Fetch all projects
+    # Fetch all projects
+    # GET /projects.json
+    #
     #   client.projects!
     #
-    #   # Fetch archived projects
+    # Fetch archived projects
+    # GET /projects/archived.json
+    #
     #   clients.projects.archived!
     #
-    #   # Fetch single project with ID of 123
+    # Fetch single project with ID of 123
+    # GET /projects/123.json
+    #
     #   client.projects!(123)
+    #
+    # Create a project
+    # POST /projects.json
+    #
+    #   client.projects.create!(name: 'Acme project', description: 'This is a new project')
+    #
+    # Update an existing project
+    # PUT /projects/123.json
+    #
+    #   client.projects(123).update!(description: 'A new description')
+    #
+    # Delete a project
+    # DELETE /projects/123.json
+    #
+    #   client.projects(123).delete!
     #
     class Project < Rapidash::Base
       root :project
