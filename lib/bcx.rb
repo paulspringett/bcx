f9a9764c481ae489493d1618e4bb27df9d6e58d8
Paul Springett <paul@springett.me>
2013-05-24 17:07:41 +0100

    Change autoload ordering

--- lib/bcx.rb
+++ lib/bcx.rb
@@ -3,13 +3,14 @@ require 'bcx/version'
 
 module Bcx
   autoload :Configuration, 'bcx/configuration'
-  autoload :Client, 'bcx/client'
 
   module Resources
-    autoload :Project, 'bcx/resources/project'
-    autoload :Todolist, 'bcx/resources/todolist'
     autoload :Todo, 'bcx/resources/todo'
+    autoload :Todolist, 'bcx/resources/todolist'
+    autoload :Project, 'bcx/resources/project'
   end
+
+  autoload :Client, 'bcx/client'
 
   class << self
     attr_accessor :configuration
