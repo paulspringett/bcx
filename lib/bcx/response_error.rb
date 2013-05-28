34f05e84f676364d561af8de11c16d6554b15f46
Paul Springett <paul@springett.me>
2013-05-28 16:15:17 +0100

    Add errors method to custom exception handler

--- lib/bcx/response_error.rb
+++ lib/bcx/response_error.rb
@@ -1,3 +1,17 @@
 module Bcx
-  class ResponseError < Rapidash::ResponseError; end
+  class ResponseError < Rapidash::ResponseError
+
+    def errors
+      return body if body.kind_of?(String)
+
+      messages = []
+
+      body.each_pair do |attribute, msgs|
+        msgs.each { |msg| messages.push "#{attribute} #{msg}" }
+      end
+
+      messages.join(', ')
+    end
+
+  end
 end
