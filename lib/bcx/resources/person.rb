ad6162e2d28ece33435807646b558052ab97e59a
Paul Springett <paul@springett.me>
2013-05-28 22:18:37 +0100

    Stub for Person resource

---
+++ lib/bcx/resources/person.rb
@@ -0,0 +1,13 @@
+module Bcx
+  module Resources
+
+    # Get todolists with assigned todos for a person
+    # GET /people/1/assigned_todos.json
+    #
+    #   client.people(1).assigned_todos
+    #
+    class Person < Rapidash::Base
+
+    end
+  end
+end
