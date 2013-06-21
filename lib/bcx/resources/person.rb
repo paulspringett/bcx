cd09b175a6abb172796181abb2409a4d26c1d3b9
Paul Springett <paul@springett.me>
2013-06-22 00:33:29 +0100

    Update Person resource docs for Docco formatting

--- lib/bcx/resources/person.rb
+++ lib/bcx/resources/person.rb
@@ -1,34 +1,33 @@
+# `Bcx::Resources::Person`
+# Provides access to people resource
+#
+# #### Get all people on the account
+# `GET /people.json`
+#
+#     client.people!
+#
+# #### Get a person
+# `GET /people/123.json`
+#
+#     client.people!(123)
+#
+# #### Get the current person
+# `GET /people/me.json`
+#
+#     client.people.me!
+#
+# #### Get assigned todos for a person
+# `GET /people/1/assigned_todos.json`
+#
+#     client.people(1).assigned_todos
+#
+# #### Delete a person
+# `DELETE /people/123.json`
+#
+#     client.people(123).delete!
+#
 module Bcx
   module Resources
-
-    # Bcx::Resources::Person
-    # Provides access to people resource
-    #
-    # Get all people on the account
-    # GET /people.json
-    #
-    #   client.people!
-    #
-    # Get a person
-    # GET /people/123.json
-    #
-    #   client.people!(123)
-    #
-    # Get the current person
-    # GET /people/me.json
-    #
-    #   client.people.me!
-    #
-    # Get todolists with assigned todos for a person
-    # GET /people/1/assigned_todos.json
-    #
-    #   client.people(1).assigned_todos
-    #
-    # Delete a person
-    # DELETE /people/123.json
-    #
-    #   client.people(123).delete!
-    #
     class Person < Rapidash::Base
       url :people
       collection :me
