f4bb26153f7537d8a0ad14e28c153c7c09ed358a
Paul Springett <paul@springett.me>
2013-07-04 22:44:18 +0100

    Docs for configuration and response error

--- lib/bcx/configuration.rb
+++ lib/bcx/configuration.rb
@@ -1,13 +1,14 @@
+# ## Configuration
+#
+# Provides a configuration block for setting up the Bcx client
+#
+# Example:
+#
+#     Bcx.configure do |config|
+#       config.account = '1234567890'
+#     end
+#
 module Bcx
-
-  # Bcx::Configuration
-  # Provides a configuration block for setting up the Bcx client
-  # Example:
-  #
-  #   Bcx.configure do |config|
-  #     config.account = '1234567890'
-  #   end
-  #
   class Configuration
     attr_accessor :account, :api_version
 
--- lib/bcx/response_error.rb
+++ lib/bcx/response_error.rb
@@ -1,3 +1,23 @@
+# ## Response Error
+#
+# If the response whilst fetching a resource is a 4xx or 5xx, Bcx will raise a `Bcx::ResponseError` exception.
+#
+# Examples:
+#
+#     client.projects.create!(name: '')
+#     => Bcx::ResponseError: 422 POST https://basecamp.com/2274488/api/v1/projects.json | Errors: name can't be blank
+#
+# You can rescue this exception to grab the status, method, URL and errors individually.
+#
+#     begin
+#       client.projects.create!(name: '')
+#     rescue Bcx::ResponseError => response
+#       response.method # => "POST"
+#       response.status # => 422
+#       response.url # => "https://basecamp.com/2274488/api/v1/projects.json"
+#       response.errors # => ["name can't be blank"]
+#     end
+#
 module Bcx
   class ResponseError < Rapidash::ResponseError
 
